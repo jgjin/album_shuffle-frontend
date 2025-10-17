8ee18f8900a7255520a097f1fa1a4cc5132438e4
Jason Jin <jgjin@umich.edu>
2025-10-17 01:21:35 -0400

    avoids refreshing album list after stale time

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import { AlbumCard } from "./components/AlbumCard"
 import { useListAlbumListGet } from "./queries/album_shuffle"
 
 export function App() {
-  const { data } = useListAlbumListGet({})
+  const { data } = useListAlbumListGet({
+    query: {
+      staleTime: Infinity,
+    },
+  })
 
   return (
     <div className="grid w-screen h-screen grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4 p-8">
