0e303a8c471e2d152894756eebe2ceca51cd6a03
Jason Jin <jgjin@umich.edu>
2025-10-19 22:09:18 -0400

    redirects to backend upon list album HTTP 401

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import { App } from "./App.tsx"
 
 import "./index.css"
 
-axios.defaults.baseURL = "http://127.0.0.1:8000"
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL
 axios.defaults.withCredentials = true
 
 createRoot(document.getElementById("root")!).render(
